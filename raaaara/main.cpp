424b5e16bf879fa3524f8459caf76e174568feb2
Sh0kerbled <d-nurgaziev@mail.ru>
2022-04-21 22:49:39 +0600

    hm

--- raaaara/main.cpp
+++ raaaara/main.cpp
@@ -192,7 +192,7 @@ int main()
         cin.clear();
         while (cin.get() != '\n')
             continue;
-        cout << "Error ";
+        cout << "it's incorrect! ";
     }
     //task 2
     cout << "Unreal" << endll << "Engine";
